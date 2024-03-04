869145f1819d0ba2cf114e99e8fb5d93de07fe1c
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-03-04 15:53:07 +0100

    pic

--- first_dev.py
+++ first_dev.py
@@ -1,17 +1,24 @@
 import requests
 
 # url = "https://api.openweathermap.org/data/2.5"
-url2 = ("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,"
-        "wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
-r = requests.get(url2)
+# url2 = ("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,"
+#         "wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+# r = requests.get(url2)
+#
+# # r1 = requests.get(url)
+#
+# print(r.text)
 
-# r1 = requests.get(url)
+# # print(r1.text)
+# data = r.json()
+# print(data['current_units']['time'])
+#
+# print(data['current_units'])
+# print(data)
 
-print(r.text)
-# print(r1.text)
-data = r.json()
-print(data['current_units']['time'])
+url3 = ("https://media.licdn.com/dms/image/D4D03AQEqAKmfIfxYVg/profile-displayphoto-shrink_800_800/0/1690304358450?e"
+        "=1715212800&v=beta&t=vUUPM722dVEkUuoQlBilCsvv5r7cWyZcwueEEjsv77g")
+r5 = requests.get(url3)
 
-print(data['current_units'])
-print(data)
-
+with open('jumong.png', mode='wb') as mf:
+    mf.write(r5.content)
